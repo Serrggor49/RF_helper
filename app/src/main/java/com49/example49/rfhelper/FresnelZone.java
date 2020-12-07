063f95e5e1dd0a0270cc40f0523b7a4647f3fd13
Serrggor49 <56230469+Serrggor49@users.noreply.github.com>
2020-12-07 19:47:02 +0300

    Рефакторинг главной страницы, прототип, без картинок

--- app/src/main/java/com49/example49/rfhelper/FresnelZone.java
+++ app/src/main/java/com49/example49/rfhelper/FresnelZone.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,76 +16,81 @@ import com49.example49.rfhelper.R;
 
 public class FresnelZone extends AppCompatActivity {
 
-
-    int descriptions_fresnel = R.string.descriptions_fresnel; // текст с описанием
-
-    EditText freqEditText;  // поле ввода частоты в МГц
-    EditText distanceEditText; // поле ввода расстояния в метрах
-    TextView textView_100; // результат 100% зоны Френеля
-    TextView textView_80; // результат 80% зоны Френеля
-    TextView textView_60; // результат 60% зоны Френеля
+    final String INPUT_ERROR = "Убедитесь в правильности заполнения полей";
+    private int descriptions_fresnel = R.string.descriptions_fresnel; // текст с описанием
+    private EditText mFreqEditText;  // поле ввода частоты в МГц
+    private EditText mDistanceEditText; // поле ввода расстояния в метрах
+    private TextView mTextView_100; // результат 100% зоны Френеля
+    private TextView mTextView_80; // результат 80% зоны Френеля
+    private TextView mTextView_60; // результат 60% зоны Френеля
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fresnel_zone);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // включает отображение стрелочки назад в тулбаре
-        //getSupportActionBar().setHomeAsUpIndicator(R.mipmap.back_orig);  // добавляем картинку клавише назад в тулбаре
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#f4fcf2'>Зона Френеля</font>"));
+        init();
 
-        TextView description = findViewById(R.id.description_id); // описание зоны Френеля
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+
+    void init() {
+        TextView description = findViewById(R.id.description_id);
         description.setText(descriptions_fresnel);
 
-
-        ImageView imageView = (ImageView) findViewById(R.id.header_id);
+        ImageView imageView = findViewById(R.id.header_id);
         imageView.setBackgroundResource(R.drawable.fresnel_animate);
+
         AnimationDrawable mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
         mAnimationDrawable.start();
 
+        Button buttonCalc = findViewById(R.id.button_calc_id);
+        buttonCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fieldsCorrect();
+            }
+        });
     }
 
-    @Override public boolean onSupportNavigateUp() { onBackPressed(); return true; } // обработка назад в toolbar
+    public void calculate() {
 
+        double distance = Double.parseDouble(mDistanceEditText.getText().toString());
+        double freq = Double.parseDouble(mFreqEditText.getText().toString());
 
-    public void refresh(){
+        Double zoneDouble_100 = (17.3) * Math.sqrt((distance / (4 * freq)));
 
-        Double distance = Double.parseDouble(distanceEditText.getText().toString());
-        Double freq = Double.parseDouble(freqEditText.getText().toString());
-
-        Double zoneDouble_100 = (17.3)*Math.sqrt( (distance/ (4*freq))  );
-
-        textView_100.setText( "100% - " + String.format("%.1f", zoneDouble_100));
-        textView_80.setText( "80% - " + (String.format("%.1f", zoneDouble_100*0.8)));
-        textView_60.setText( "60% - " + (String.format("%.1f", zoneDouble_100*0.6)));
-
+        mTextView_100.setText("100% - " + String.format("%.1f", zoneDouble_100));
+        mTextView_80.setText("80% - " + (String.format("%.1f", zoneDouble_100 * 0.8)));
+        mTextView_60.setText("60% - " + (String.format("%.1f", zoneDouble_100 * 0.6)));
     }
 
+    public void fieldsCorrect() {  // проверка правильности заполнения полей
+
+        mFreqEditText = findViewById(R.id.freq_text_edit_id);  // поле ввода частоты в МГц
+        mDistanceEditText = findViewById(R.id.distance_text_edit_id); // поле ввода расстояния в метрах
+        mTextView_100 = findViewById(R.id.radius_100_id); // результат 100% зоны Френеля
+        mTextView_80 = findViewById(R.id.radius_80_id); // результат 80% зоны Френеля
+        mTextView_60 = findViewById(R.id.radius_60_id); // результат 60% зоны Френеля
 
 
-    public void fieldsCorrect(View view){  // проверка правильности заполнения полей
-
-
-        freqEditText = findViewById(R.id.freq_text_edit_id);  // поле ввода частоты в МГц
-        distanceEditText = findViewById(R.id.distance_text_edit_id); // поле ввода расстояния в метрах
-        textView_100 = findViewById(R.id.radius_100_id); // результат 100% зоны Френеля
-        textView_80 = findViewById(R.id.radius_80_id); // результат 80% зоны Френеля
-        textView_60 = findViewById(R.id.radius_60_id); // результат 60% зоны Френеля
-
-
-        try
-        {
+        try {
             //double distance = Double.parseDouble(distanceEditText.getText().toString());
             //double freq = Double.parseDouble(freqEditText.getText().toString());
-            refresh();
+            calculate();
 
         } catch (Exception e1) {
             e1.printStackTrace();
-            Toast.makeText(this, "Убедитесь в правильности заполнения полей", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, INPUT_ERROR, Toast.LENGTH_LONG).show();
 
         }
-
-
 
 
     }
--- app/src/main/java/com49/example49/rfhelper/MainActivity.java
+++ app/src/main/java/com49/example49/rfhelper/MainActivity.java
@@ -3,178 +3,75 @@ package com49.example49.rfhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Point;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.Html;
-import android.view.Display;
-import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com49.example49.rfhelper.R;
-
-import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
-    int WIDTH_DISPLAY; // ширина дисплея
-    int BUTON_HEIGHT; // высота кнопки для квадратного изображения. Определяется шириной дисплея
-    int HEIGHT_COEFFICIENT = 3; // данный коэффициент показывает, какую часть от ширины экрана будет занимать кнопка (например, одна треть)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initButton();
+    }
 
-        getSupportActionBar().setTitle(Html.fromHtml("<font color='#f4fcf2'>RF helper</font>"));
+    private void initButton() {
+        ImageButton buttonFresnelZone = findViewById(R.id.button_fresnel_id);
+        buttonFresnelZone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FresnelZone.class);
+                startActivity(intent);
+            }
+        });
 
+        ImageButton buttonDalnost = findViewById(R.id.button_dalnost_id);
+        buttonDalnost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DistanceGorizont.class);
+                startActivity(intent);
+            }
+        });
 
-        Display display = getWindowManager().getDefaultDisplay();
-        Point size = new Point();
-        display.getSize(size);
-        WIDTH_DISPLAY = size.x;
-        BUTON_HEIGHT = WIDTH_DISPLAY / HEIGHT_COEFFICIENT;
+        ImageButton buttonZatuhanie = findViewById(R.id.button_zatuhanie_id);
+        buttonZatuhanie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignalLossDistance.class);
+                startActivity(intent);
+            }
+        });
 
+        ImageButton buttonSwrToDb = findViewById(R.id.button_ksv_to_db_id);
+        buttonSwrToDb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SwrToPower.class);
+                startActivity(intent);
+            }
+        });
 
-        buttons_generate(R.drawable.fresnel, "Расчет зоны Френеля", openFresnel);
-        buttons_generate(R.drawable.radiovidimost, "Дальность видимости", openDistanceHorizont);
-        buttons_generate(R.drawable.signal_zatuhanie, "Затухание сигнала в пространстве", openSignalLossDistance);
-        buttons_generate(R.drawable.antenna, "КСВ -> дБ", openSwrToPower);
-        buttons_generate(R.drawable.watts, "дБмВт -> Вт", openDecibelToWatt);
-        buttons_generate(R.drawable.waves, "Длина волны по частоте", openFreqToLenght);
-        //buttons_generate(R.drawable.battery, "Таблица частот (в разработке)", openDecibelToWatt);
+        ImageButton buttonDbmToWt = findViewById(R.id.button_dbm_to_vt_id);
+        buttonDbmToWt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DecibelToWatt.class);
+                startActivity(intent);
+            }
+        });
 
+        ImageButton buttonFreqToLength = findViewById(R.id.button_lenght_id);
+        buttonFreqToLength.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FreqToLenght.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
 
-    View.OnClickListener openFresnel = new View.OnClickListener() {  // калькулятор Френеля
-        @Override
-        public void onClick(View v) {
-
-            startActivity(new Intent(MainActivity.this, FresnelZone.class));
-
-
-        }
-    };
-
-
-    View.OnClickListener openDistanceHorizont = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, DistanceGorizont.class));
-        }
-    };
-
-
-    View.OnClickListener openSignalLossDistance = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-
-            startActivity(new Intent(MainActivity.this, SignalLossDistance.class));
-
-        }
-    };
-
-
-    View.OnClickListener openSwrToPower = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-
-            startActivity(new Intent(MainActivity.this, SwrToPower.class));
-
-        }
-    };
-
-    // https://tilda.cc/page/?pageid=12216270&previewmode=yes
-
-
-    View.OnClickListener openDecibelToWatt = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, DecibelToWatt.class));
-        }
-    };
-
-
-    View.OnClickListener openFreqToLenght = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-            startActivity(new Intent(MainActivity.this, FreqToLenght.class));
-        }
-    };
-
-
-    View.OnClickListener method_vita = new View.OnClickListener() {  // обработка кнопки "Литература"
-        @Override
-        public void onClick(View v) {
-//            URL_obshee.open_in_apk = true;
-//            URL_obshee.URL = ("https://justveg.ru/vita");
-//            URL_obshee.activity_name = "Vita_news";
-//            URL_obshee.setTitle("<font color='#18450e'>Новости ВИТА</font>");
-//            startActivity(new Intent(Other_menu_new.this, URL_obshee.class));
-        }
-    };
-
-
-    public void buttons_generate(int image, String name_button, View.OnClickListener method) {
-
-        int LINE_PADDING = 20; // отступы от кнопки до разделительной линии (сверху и знизу)
-        int TEXT_ZAGOLOVOK_SIZE = 20;  // размер текста заголовков
-        int PADDING_TEXT_LEFT = 10; // отступ текста слева
-        int PADDING_TEXT_RIGHT = 0; // отступ текста справа
-        int PADDING_TEXT_TOP = 5; // отступ между заголовком и описательным текстом
-        int PADDING_TEXT_BOTTOM = 10; // отступ между заголовком и описательным текстом
-        float RADIUS = 0.2f; // радиус скругления углов кнопок
-
-        LinearLayout layout = findViewById(R.id.lin_o);
-
-        LinearLayout layoutButton = new LinearLayout(this); // создали LinearLayout в который будем помещать картинку и тексты
-        LinearLayout.LayoutParams params_layoutButton = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, BUTON_HEIGHT); // максимальная ширина кнопки и ее высота которую мы выбираем сами
-        layoutButton.setLayoutParams(params_layoutButton);
-        //layoutButton.setBackgroundColor(getColor(R.color.text_color_primary_3));  // определили цвет для кнопки
-        layoutButton.setBackground(getDrawable(R.drawable.ripple));  // определили цвет для кнопки
-
-        ImageView imageView = new ImageView(this);  // создали ImageView в который помещаем картинку для кнопки
-        LinearLayout.LayoutParams params_image = new LinearLayout.LayoutParams(WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        imageView.setLayoutParams(params_image);
-        Drawable d = getDrawable(image);
-        //d = new Rounded_corners().setEffect(d, RADIUS, layoutButton.getSolidColor()); // необходимо, чтобы места скругления имели тот же цвет, что и фон
-        d = new Rounded_corners().setEffect(d, RADIUS, getColor(R.color.text_color_primary_3)); // необходимо, чтобы места скругления имели тот же цвет, что и фон
-        imageView.setImageDrawable(d);
-        imageView.setAdjustViewBounds(true);
-
-        TextView textView_zagolovok = new TextView(this);
-        textView_zagolovok.setPadding(PADDING_TEXT_LEFT, PADDING_TEXT_TOP, PADDING_TEXT_RIGHT, PADDING_TEXT_BOTTOM);
-        textView_zagolovok.setText(name_button);
-        textView_zagolovok.setTypeface(Typeface.DEFAULT_BOLD);
-        textView_zagolovok.setTextColor(getColor(R.color.Black));
-        textView_zagolovok.setTextSize(TEXT_ZAGOLOVOK_SIZE);
-        textView_zagolovok.setGravity(Gravity.CENTER_VERTICAL);
-        textView_zagolovok.setPadding(25, 0, 0, 0);
-
-        LinearLayout.LayoutParams params_for_text1 = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, MATCH_PARENT);
-        textView_zagolovok.setLayoutParams(params_for_text1);
-
-        layoutButton.addView(imageView);
-        layoutButton.addView(textView_zagolovok);
-        layoutButton.setOnClickListener(method);
-
-        View line = new View(this);
-        LinearLayout.LayoutParams params_for_line = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 3);
-        params_for_line.setMargins(0, LINE_PADDING, 0, LINE_PADDING);
-        line.setLayoutParams(params_for_line);
-        line.setBackgroundColor(Color.parseColor("#B3B3B3"));
-
-        layout.addView(layoutButton);
-        layout.addView(line);
-
-    }
 }
