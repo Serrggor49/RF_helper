e52f54b85682f3b5d61a464beaf4c33383e0ecf1
Serrggor49 <56230469+Serrggor49@users.noreply.github.com>
2020-12-07 23:24:43 +0300

    Рефакторинг SignalLossDistance

--- app/src/main/java/com49/example49/rfhelper/SignalLossDistance.java
+++ app/src/main/java/com49/example49/rfhelper/SignalLossDistance.java
@@ -6,73 +6,75 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com49.example49.rfhelper.R;
-
 public class SignalLossDistance extends AppCompatActivity {
 
-    int descriptionsSignalLossDistance = R.string.signal_loss_distance; // текст с описанием
-    EditText freqEditText;  // поле ввода частоты сигнала
-    EditText distanceEditText;  // поле ввода частоты сигнала
-    TextView textView_result; // результат расчета
-    TextView description;
-
+    private String INPUT_ERROR = "Убедитесь в правильности заполнения полей";
+    private int descriptionsSignalLossDistance = R.string.signal_loss_distance; // текст с описанием
+    private EditText freqEditText;  // поле ввода частоты сигнала
+    private EditText distanceEditText;  // поле ввода частоты сигнала
+    private TextView textView_result; // результат расчета
+    private TextView description;
+    private Button mButtonCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signal_loss_distance);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);  // включает отображение стрелочки назад в тулбаре
-        //getSupportActionBar().setHomeAsUpIndicator(R.mipmap.back_orig);  // добавляем картинку клавише назад в тулбаре
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#f4fcf2'>Затухание сигнала</font>"));
-
-
-        ImageView imageView = (ImageView) findViewById(R.id.header_id);
-        imageView.setBackgroundResource(R.drawable.zatuhanie_animate);
-        AnimationDrawable mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
-        mAnimationDrawable.start();
-
-        description = findViewById(R.id.description_id); // добавили описание что такое радиогоризонт
-        description.setText(descriptionsSignalLossDistance);
-    }
-
-    @Override public boolean onSupportNavigateUp() { onBackPressed(); return true; } // обработка назад в toolbar
-
-
-    public void refresh() {
-
-        Double freq = Double.parseDouble(freqEditText.getText().toString());
-        Double distance = Double.parseDouble(distanceEditText.getText().toString());
-
-        Double result = (32.4 + 20 * Math.log10(freq) + 20 * Math.log10(distance));
-
-        textView_result.setText("Затухание сигнала составляет - " + (String.format("%.1f", result)) + " дБ.");
+        init();
+        calculate();
 
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 
-    public void fieldsCorrect(View view) {  // проверка правильности заполнения полей
-
+    void init() {
 
         freqEditText = findViewById(R.id.freq_id);  // частота
         distanceEditText = findViewById(R.id.distance_id); // расстояние
         textView_result = findViewById(R.id.loss_id); // результат
 
+        ImageView imageView = findViewById(R.id.header_id);
+        imageView.setBackgroundResource(R.drawable.zatuhanie_animate);
+
+        AnimationDrawable mAnimationDrawable = (AnimationDrawable) imageView.getBackground();
+        mAnimationDrawable.start();
+
+        description = findViewById(R.id.description_id); // добавили описание что такое радиогоризонт
+        description.setText(descriptionsSignalLossDistance);
+
+        mButtonCalc = findViewById(R.id.button_calc_id);
+        mButtonCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
+
+    }
+
+    public void calculate() {
 
         try {
-            //double distance = Double.parseDouble(distanceEditText.getText().toString());
-            //double freq = Double.parseDouble(freqEditText.getText().toString());
-            refresh();
-
-        } catch (Exception e1) {
-            e1.printStackTrace();
-            Toast.makeText(this, "Убедитесь в правильности заполнения полей", Toast.LENGTH_LONG).show();
-
+            double freq = Double.parseDouble(freqEditText.getText().toString());
+            double distance = Double.parseDouble(distanceEditText.getText().toString());
+            Double result = (32.4 + 20 * Math.log10(freq) + 20 * Math.log10(distance));
+            textView_result.setText("Затухание сигнала составляет - " + (String.format("%.1f", result)) + " дБ.");
+        } catch (NumberFormatException e) {
+            Toast.makeText(this, INPUT_ERROR, Toast.LENGTH_LONG).show();
         }
 
     }
+
 }
